9fe7958480a02f707078c40ba83d60027628b8cc
Ildar <ildar.1795@gmail.com>
2025-03-28 19:17:44 +0400

    feat: добавляет мок данные для тестов аналитики

---
+++ src/tests/mocks/mocks.ts
@@ -0,0 +1,12 @@
+export const mockAnalyticsEvents = [
+  {
+    timestamp: 6.160356073346696,
+    duration: 0.8361136523432808,
+    zone: {
+      left: 113.29959866169601,
+      top: 195.3639952425215,
+      width: 126.18979937751924,
+      height: 46.23090211142281
+    }
+  },
+]
